45baafcac66dedd640b6496ceb853e094ac95bb8
Nixon Silva <nixonmoreira42@gmail.com>
2020-10-02 22:04:00 -0300

    Movement Controller
    Initial mplementation of a simple 2D Top-Down (8 orthogonal directions) movement system.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private AudioManager audioManager;
+
+    private Rigidbody2D rb;
+    private CircleCollider2D col;
+
+    private Vector2 movementVector;
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private void Awake ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate ()
+    {
+        PerformMovement(movementVector, speed);
+    }
+
+    private void PerformMovement (Vector2 direction, float speed)
+    {
+        Vector2 movement = direction * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+
+    public void ForceMove (Vector2 direction, float speed)
+    {
+        PerformMovement(direction, speed);
+    }
+}
