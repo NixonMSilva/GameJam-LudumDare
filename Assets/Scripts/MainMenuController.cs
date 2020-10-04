b4e58ca9604029152cdb1715466c0c44d21d8220
Nixon Silva <nixonmoreira42@gmail.com>
2020-10-04 18:18:09 -0300

    Main Menu
    Implemented the template for the main menu, UI graphics will later be added to this base.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject mainButtons;
+    public GameObject instructionBG;
+
+    public void PlayGame ()
+    {
+        Debug.Log("Play game");
+        Time.timeScale = 1f;
+    }
+
+    public void ShowInstructions ()
+    {
+        instructionBG.SetActive(true);
+        mainButtons.SetActive(false);
+    }
+
+    public void HideInstructions ()
+    {
+        instructionBG.SetActive(false);
+        mainButtons.SetActive(true);
+    }
+
+    public void ExitGame ()
+    {
+        Application.Quit();
+    }
+}
